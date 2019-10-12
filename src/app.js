5098a955668a68934e9a4dd7548b78dc78bc8bc5
CooldeGitHub <3346695454@qq.com>
2019-10-12 14:32:38 +0800

    配置vue router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,43 @@
 import css from './app.css'
-import Vue from 'vue'
-import App from './app.vue'
+import Vue from 'vue/dist/vue.common.js'
+import VueRouter from 'vue-router'
+import App from './components/app.vue'
 
+Vue.use(VueRouter)
 console.log('hello world3')
 
-new Vue(App).$mount('#app')
+//1.创建组件
+const Home = {
+    template : `<div>
+    <span>this is home</span>
+</div>`
+}
+
+const User = {
+    template : `<div>
+                    <span>this is user</span>
+                </div>`
+}
+
+//2.配置路由
+const Routers = [
+    {
+        path:'/home' , component : Home
+    },
+    {
+        path:'/user' , component : User
+    },
+    {
+        path:'*' , redirect: '/home'
+    }
+]
+//3.创建路由实例
+var router = new VueRouter({
+    routes : Routers
+})
+
+var vm = new Vue({
+    el:'#app',
+    render: c=>c(App),
+    router
+})
---
+++ src/components/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link to="/home">to home</router-link>
+    <router-link to="/user">to user</router-link>
+    <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    data : function(){
+        return {
+            post:'Hello Vue-Webpack'
+        }
+    }
+}
+</script>
