a258f3534dd4dec2912d4c7bf0fdcee2adef0ca1
yepengjie <3346695454@qq.com>
2019-10-13 17:40:31 +0800

    尝试动态参数路由

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: pengjie.ye
+ * @Date: 2019-10-13 16:34:53
+ * @LastEditors: pengjie.ye
+ * @LastEditTime: 2019-10-13 17:39:01
+ */
 import css from './app.css'
 import Vue from 'vue/dist/vue.common.js'
 import VueRouter from 'vue-router'
@@ -7,28 +15,33 @@ Vue.use(VueRouter)
 console.log('hello world3')
 
 //1.创建组件
+
 const Home = {
     template : `<div>
-    <span>this is home</span>
-</div>`
+                <span>this is home</span>
+                <span>dynamic segment {{$route.params.id}}</span>
+                </div>`
+                //注意获取动态参数时是 params 有s 下同
 }
 
 const User = {
     template : `<div>
                     <span>this is user</span>
+                    <span>dynamic segment {{$route.params.id}}</span>
                 </div>`
 }
 
 //2.配置路由
 const Routers = [
     {
-        path:'/home' , component : Home
+        path:'/home/:id' , component : Home
     },
     {
-        path:'/user' , component : User
+        path:'/user/:id' , component : User
     },
-    {
-        path:'*' , redirect: '/home'
+    {   
+        //默认路径
+        path:'*' , redirect: '/home/home_1'
     }
 ]
 //3.创建路由实例
--- src/components/app.vue
+++ src/components/app.vue
@@ -1,7 +1,17 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: pengjie.ye
+ * @Date: 2019-10-13 16:34:53
+ * @LastEditors: pengjie.ye
+ * @LastEditTime: 2019-10-13 17:39:55
+ -->
 <template>
     <div>
-        <router-link to="/home">to home</router-link>
-    <router-link to="/user">to user</router-link>
+        <router-link to="/home/home_1">to home1</router-link>
+        <router-link to="/home/home_2">to home2</router-link>
+        <router-link to="/user/user_1">to user1</router-link>
+        <router-link to="/user/user_2">to user2</router-link>
     <router-view></router-view>
     </div>
 </template>
